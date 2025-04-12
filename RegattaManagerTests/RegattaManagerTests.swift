a09aa9a7aab94474b274259b05d6f0de42452d97
Neil Fulcher <neilfulcher@e.com>
2025-04-12 21:35:54 +0100

    Initial Commit

---
+++ RegattaManagerTests/RegattaManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RegattaManagerTests.swift
+//  RegattaManagerTests
+//
+//  Created by Neil Fulcher on 12/04/2025.
+//
+
+import Testing
+@testable import RegattaManager
+
+struct RegattaManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
